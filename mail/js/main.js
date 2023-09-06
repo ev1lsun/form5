d3c4615095d979c4a8fc6b206e7dd46e8f0ea684
Denis <36629134+ev1lsun@users.noreply.github.com>
2023-09-06 22:03:08 +0300

    Update main.js

--- mail/js/main.js
+++ mail/js/main.js
@@ -1,37 +1,47 @@
-this.payment = function () {
-    var payments = new cp.CloudPayments({
-    language: "ru-RU",
-    email: "",
-    applePaySupport: false,
-    googlePaySupport: false,
-    yandexPaySupport: true,
-    tinkoffInstallmentSupport: true,
-});
+this.pay = function () {
+    var widget = new cp.CloudPayments();
+    var receipt = {
+            Items: [//товарные позиции
+                 {
+                    label: 'Наименование товара 3', //наименование товара
+                    price: 300.00, //цена
+                    quantity: 3.00, //количество
+                    amount: 900.00, //сумма
+                    vat: 20, //ставка НДС
+                    method: 0, // тег-1214 признак способа расчета - признак способа расчета
+                    object: 0, // тег-1212 признак предмета расчета - признак предмета товара, работы, услуги, платежа, выплаты, иного предмета расчета
+                }
+            ],
+            taxationSystem: 0, //система налогообложения; необязательный, если у вас одна система налогообложения
+            email: 'user@example.com', //e-mail покупателя, если нужно отправить письмо с чеком
+            phone: '', //телефон покупателя в любом формате, если нужно отправить сообщение со ссылкой на чек
+            isBso: false, //чек является бланком строгой отчетности
+            amounts:
+            {
+                electronic: 900.00, // Сумма оплаты электронными деньгами
+                advancePayment: 0.00, // Сумма из предоплаты (зачетом аванса) (2 знака после запятой)
+                credit: 0.00, // Сумма постоплатой(в кредит) (2 знака после запятой)
+                provision: 0.00 // Сумма оплаты встречным предоставлением (сертификаты, др. мат.ценности) (2 знака после запятой)
+            }
+        };
 
-    var data = { //данные дарителя
-        name: $('#name_field').val(),
-        text: $('#name_field').val(),
-        phone: $('#phone_field').val()
-    };
+    var data = {};
+    data.CloudPayments = {
+        CustomerReceipt: receipt, //чек для первого платежа
+        recurrent: {
+         interval: 'Month',
+         period: 1, 
+         customerReceipt: receipt //чек для регулярных платежей
+         }
+         }; //создание ежемесячной подписки
 
-    var auto = $('#check').is(':checked'); //проверка
-
-    if (auto) { //включаем подписку
-        data.CloudPayments = {
-            recurrent: { interval: 'Month', period: 1 } //один раз в месяц начиная со следующего месяца
-        }
-    }
-
-
-    var accountId = $('#email-field').val();
-
-    payments.pay("charge", { // options
+    widget.charge({ // options
         publicId: 'pk_3964da3d612302cfbf41b94414ec6', //id из личного кабинета
-        description: 'Пожертвование в фонд тестов', //назначение
-        amount: 100, //сумма
+        description: 'Подписка на ежемесячный доступ к сайту example.com', //назначение
+        amount: 1000, //сумма
         currency: 'RUB', //валюта
-        accountId: accountId, //идентификатор плательщика (обязательно для создания подписки)
-        email: accountId,
+        invoiceId: '1234567', //номер заказа  (необязательно)
+        accountId: 'user@example.com', //идентификатор плательщика (обязательно для создания подписки)
         data: data
     },
     function (options) { // success
