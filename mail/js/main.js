b8d51407c3724819e867069c1b46340f082ef6ba
Denis <36629134+ev1lsun@users.noreply.github.com>
2023-09-06 22:10:41 +0300

    Update main.js

--- mail/js/main.js
+++ mail/js/main.js
@@ -1,56 +1,29 @@
-this.pay = function () {
-    var widget = new cp.CloudPayments();
-    var receipt = {
-            Items: [//товарные позиции
-                 {
-                    label: 'Наименование товара 3', //наименование товара
-                    price: 300.00, //цена
-                    quantity: 3.00, //количество
-                    amount: 900.00, //сумма
-                    vat: 20, //ставка НДС
-                    method: 0, // тег-1214 признак способа расчета - признак способа расчета
-                    object: 0, // тег-1212 признак предмета расчета - признак предмета товара, работы, услуги, платежа, выплаты, иного предмета расчета
-                }
-            ],
-            taxationSystem: 0, //система налогообложения; необязательный, если у вас одна система налогообложения
-            email: 'user@example.com', //e-mail покупателя, если нужно отправить письмо с чеком
-            phone: '', //телефон покупателя в любом формате, если нужно отправить сообщение со ссылкой на чек
-            isBso: false, //чек является бланком строгой отчетности
-            amounts:
-            {
-                electronic: 900.00, // Сумма оплаты электронными деньгами
-                advancePayment: 0.00, // Сумма из предоплаты (зачетом аванса) (2 знака после запятой)
-                credit: 0.00, // Сумма постоплатой(в кредит) (2 знака после запятой)
-                provision: 0.00 // Сумма оплаты встречным предоставлением (сертификаты, др. мат.ценности) (2 знака после запятой)
-            }
-        };
+var payments = new cp.CloudPayments({
+    language: "ru-RU",
+    email: "",
+    applePaySupport: true,
+    googlePaySupport: true,
+    yandexPaySupport: true,
+    tinkoffInstallmentSupport: true,
+});
 
-    var data = {};
-    data.CloudPayments = {
-        CustomerReceipt: receipt, //чек для первого платежа
-        recurrent: {
-         interval: 'Month',
-         period: 1, 
-         customerReceipt: receipt //чек для регулярных платежей
-         }
-         }; //создание ежемесячной подписки
 
-    widget.charge({ // options
-        publicId: 'pk_3964da3d612302cfbf41b94414ec6', //id из личного кабинета
-        description: 'Подписка на ежемесячный доступ к сайту example.com', //назначение
-        amount: 1000, //сумма
-        currency: 'RUB', //валюта
-        invoiceId: '1234567', //номер заказа  (необязательно)
-        accountId: 'user@example.com', //идентификатор плательщика (обязательно для создания подписки)
-        data: data
-    },
-    function (options) { // success
-        //действие при успешной оплате
-    },
-    function (reason, options) { // fail
-        //действие при неуспешной оплате
-    });
-};
+payments.pay("charge", {
+    publicId: "test_api_00000000000000000000002",
+    description: "Тестовая оплата",
+    amount: 100,
+    currency: "RUB",
+    invoiceId: "123",
+    accountId: "123",
+    email: "",
+    skin: "classic",
+    requireEmail: false,
+}).then(function(widgetResult) {
+    console.log('result', widgetResult);
+}).catch(function(error) {
+    console.log('error', error);
+});
+      
    
 $('#payment').click(pay);
 
