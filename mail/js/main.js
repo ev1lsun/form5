a35ec2d0e45a52a5566c494acaba5482b35309e6
Denis <36629134+ev1lsun@users.noreply.github.com>
2023-09-06 21:57:40 +0300

    Update main.js

--- mail/js/main.js
+++ mail/js/main.js
@@ -1,5 +1,12 @@
 this.payment = function () {
-    var widget = new cp.CloudPayments();
+    var payments = new cp.CloudPayments({
+    language: "ru-RU",
+    email: "",
+    applePaySupport: false,
+    googlePaySupport: false,
+    yandexPaySupport: true,
+    tinkoffInstallmentSupport: true,
+});
 
     var data = { //данные дарителя
         name: $('#name_field').val(),
@@ -15,13 +22,13 @@ this.payment = function () {
         }
     }
 
-    var amount = parseFloat($('#amount-sample-4').val());
+
     var accountId = $('#email-field').val();
 
-    widget.charge({ // options
+    payments.pay("charge", { // options
         publicId: 'pk_3964da3d612302cfbf41b94414ec6', //id из личного кабинета
         description: 'Пожертвование в фонд тестов', //назначение
-        amount: 1, //сумма
+        amount: 100, //сумма
         currency: 'RUB', //валюта
         accountId: accountId, //идентификатор плательщика (обязательно для создания подписки)
         email: accountId,
@@ -37,29 +44,4 @@ this.payment = function () {
    
 $('#payment').click(payment);
 
-var payments = new cp.CloudPayments({
-    language: "ru-RU",
-    email: "",
-    applePaySupport: false,
-    googlePaySupport: false,
-    yandexPaySupport: false,
-    tinkoffInstallmentSupport: false,
-});
 
-
-payments.pay("charge", {
-    publicId: "test_api_00000000000000000000002",
-    description: "Тестовая оплата",
-    amount: 100,
-    currency: "RUB",
-    invoiceId: "123",
-    accountId: "123",
-    email: "",
-    skin: "classic",
-    requireEmail: false,
-}).then(function(widgetResult) {
-    console.log('result', widgetResult);
-}).catch(function(error) {
-    console.log('error', error);
-});
-                            
