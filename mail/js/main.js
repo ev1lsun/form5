200498335beba4898ab334b76a1bf4404432a8a8
Denis <36629134+ev1lsun@users.noreply.github.com>
2023-09-07 16:26:58 +0300

    Update main.js

--- mail/js/main.js
+++ mail/js/main.js
@@ -2,9 +2,9 @@ this.paySample4 = function () {
     var widget = new cp.CloudPayments();
 
     var data = { //данные дарителя
-        name: $('#name-sample-4').val(),
-        lastName: $('#lastName-sample-4').val(),
-        phone: $('#phone-sample-4').val()
+        name: $('#name_field').val(),
+        textarea: $('#textarea_field').val(),
+        phone: $('#phone_field').val()
     };
 
     var auto = $('#recurrent-sample-4').is(':checked'); //проверка
@@ -15,13 +15,13 @@ this.paySample4 = function () {
         }
     }
 
-    var amount = parseFloat($('#amount-sample-4').val());
-    var accountId = $('#email-sample-4').val();
+   var amount = parseFloat($('#amount-field').val());
+    var accountId = $('#email-field').val();
 
     widget.charge({ // options
         publicId: 'pk_3964da3d612302cfbf41b94414ec6', //id из личного кабинета
         description: 'Пожертвование в фонд ...', //назначение
-        amount: amount, //сумма
+        amount: 250, //сумма
         currency: 'RUB', //валюта
         accountId: accountId, //идентификатор плательщика (обязательно для создания подписки)
         email: accountId,
