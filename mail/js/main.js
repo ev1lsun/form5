16c4e2c45b06170f0fd59dd23cb9202f3cca8ef6
Denis <36629134+ev1lsun@users.noreply.github.com>
2023-09-07 13:57:08 +0300

    Update main.js

--- mail/js/main.js
+++ mail/js/main.js
@@ -1,3 +1,27 @@
+var payments = new cp.CloudPayments({
+    language: "ru-RU",
+    email: "",
+    applePaySupport: true,
+    googlePaySupport: true,
+    yandexPaySupport: true,
+    tinkoffInstallmentSupport: true,
+})
 
+
+payments.pay("charge", {
+    publicId: "pk_3964da3d612302cfbf41b94414ec6",
+    description: "Тестовая оплата",
+    amount: 100,
+    currency: "RUB",
+    invoiceId: "123",
+    accountId: "123",
+    email: "",
+    skin: "classic",
+    requireEmail: false,
+}).then(function(widgetResult) {
+    console.log('result', widgetResult);
+}).catch(function(error) {
+    console.log('error', error);
+})
 
 
