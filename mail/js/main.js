14c97f6cc73120d3fbe42ed962e15c3fecf8e2f8
Denis <36629134+ev1lsun@users.noreply.github.com>
2023-12-04 12:22:58 +0300

    Update main.js

--- mail/js/main.js
+++ mail/js/main.js
@@ -1,27 +1,64 @@
 this.paySample4 = function () {
     var widget = new cp.CloudPayments();
 
-    var data = { //данные дарителя
-        name: $('#name_field').val(),
-        textarea: $('#textarea_field').val(),
-        phone: $('#phone_field').val()
-    };
-
-    var auto = $('#check').is(':checked'); //проверка
-
-    if (auto) { //включаем подписку
-        data.CloudPayments = {
-            recurrent: { interval: 'Month', period: 1 } //один раз в месяц начиная со следующего месяца
-        }
+   var amount = parseFloat($('#amount-field').val());
+   var accountId = $('#email-field').val();
+   
+   var receipt = {
+    Items: [//товарные позиции
+         {
+            label: 'Название товара', //наименование товара
+            price: amount, //цена
+            quantity: 1, //количество
+            amount: amount, //сумма
+            vat: null, //ставка НДС
+            method: 1, // тег-1214 признак способа расчета - признак способа расчета
+            object: 0,
+            AgentSign: 0,
     }
+    ],
+    taxationSystem: 2, //система налогообложения; необязательный, если у вас одна система налогообложения
+    email: 'denisberez@gmail.com', //e-mail покупателя, если нужно отправить письмо с чеком
+    phone: '79187774422', //телефон покупателя в любом формате, если нужно отправить сообщение со ссылкой на чек
+    isBso: false, //чек является бланком строгой отчетности
+    amounts:
+    {
+        electronic: amount, // Сумма оплаты электронными деньгами
+        advancePayment: 0.00, // Сумма из предоплаты (зачетом аванса) (2 знака после точки)
+        credit: 0.00, // Сумма постоплатой(в кредит) (2 знака после точки)
+        provision: 0.00 // Сумма оплаты встречным предоставлением (сертификаты, др. мат.ценности) (2 знака после точки)
+    }
+}; 
 
- var amount = parseFloat($('#amount-field').val());
-    var accountId = $('#email-field').val();
+var auto = $('#check').is(':checked'); //проверка
+
+var data = {
+    name: $('#name_field').val(),
+    textarea: $('#textarea_field').val(),
+    phone: $('#phone_field').val(),
+};
+
+if (auto) { data.CloudPayments = {
+    CustomerReceipt: receipt, //чек для первого платежа
+    recurrent: {
+     interval: 'Month',
+     period: 1, //чек для регулярных платежей
+     }
+    }
+     }
+ 
+     else  {
+     data.CloudPayments = {
+         CustomerReceipt: receipt
+    }
+    
+  };
 
     widget.charge({ // options
-        publicId: 'pk_3964da3d612302cfbf41b94414ec6', //id из личного кабинета
+        publicId: 'pk_27d390d79b145a07a419b00f0c619', //id из личного кабинета
         description: 'Пожертвование в фонд ...', //назначение
-        amount: amount, //сумма
+        amount: amount,
+        invoiceId: '370', //сумма
         currency: 'RUB', //валюта
         accountId: accountId, //идентификатор плательщика (обязательно для создания подписки)
         email: accountId,
